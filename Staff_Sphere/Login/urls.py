c4b8347b34e819bab6d9fc0f8ced05ccac7b7858
Pintu Verma <vermapintu3d@gmail.com>
2023-11-30 13:52:21 +0530

    signuppage added

--- Staff_Sphere/Login/urls.py
+++ Staff_Sphere/Login/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('', views.login, name="loginpage"),
+    path('signup/', views.signup, name="signup"),
     ]
--- Staff_Sphere/Login/views.py
+++ Staff_Sphere/Login/views.py
@@ -8,3 +8,8 @@ def login(request):
     template = loader.get_template("loginpage.html")
 
     return HttpResponse(template.render())
+
+def signup(request):
+    template = loader.get_template("signup.html")
+
+    return HttpResponse(template.render())
