28abb1e119bfd2a9edfed55de765c728b01205f6
Pintu Verma <vermapintu3d@gmail.com>
2023-12-06 13:52:23 +0530

    added model and html file

--- Staff_Sphere/Login/models.py
+++ Staff_Sphere/Login/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Test(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  email = models.CharField(max_length=255, null=True)
+  mobile_no = models.IntegerField(null=True)
+  DOB = models.DateField(null=True)
+  gender = models.CharField(max_length=1, null=True)
+
